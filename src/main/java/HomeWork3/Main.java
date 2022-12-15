739add2b1728f1f9c55ad04b3e9ef5c24428dbae
darafomina <darafomina13403@gmail.com>
2022-12-15 22:22:57 +0300

    15.12.2022

--- src/main/java/HomeWork3/Main.java
+++ src/main/java/HomeWork3/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ex1();
-        /*ex2();*/
+        ex2();
 
     }
         private static void ex1() {
